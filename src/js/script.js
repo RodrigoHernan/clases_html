5ac5717793e03b0d9aed7c76436bfa63dd158830
Dana Julia <danajuliamehle@gmail.com>
2023-04-13 19:40:07 -0300

    kpb

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+console.log("hellow world")
+
+var mivariable;
+mivariable = "Dana";
+
+console.log(mivariable);
+
+mivariable = 1;
+
+console.log(mivariable);
+
+
+
+function porquenosabia(){
+  console.log("hola amor")
+  const elemento = document.getElementById("cualquiera");
+  mivariable = mivariable + 1
+  elemento.innerHTML = mivariable ;
+
+}
+
+var mivariable2 = 99;
+
+function eto() {
+  const elemento = document.getElementById("cualquiera2");
+  console.log("aquiestoy")
+  mivariable2 = mivariable2 - 1
+  elemento.innerHTML = mivariable2
+}
